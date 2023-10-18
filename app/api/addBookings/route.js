e45b316f565d9fa0ec2e3fd5fd816ecb8b4ee2d7
Kailash Choudhary <kailash_choudhary@srmap.edu.in>
2023-10-18 10:50:17 +0530

    Added toasts and refund info and logic

--- app/api/addBookings/route.js
+++ app/api/addBookings/route.js
@@ -32,58 +32,64 @@ export async function PUT(request) {
   const body = await request.json();
 
   const { id, bookingDetails } = body;
-  if (bookingDetails.isBooked) {
-    let overlap = false;
-
-    const room = await Room.findById(bookingDetails.room_id);
-    if (room.allBookings.length > 0) {
-      room.allBookings.map((booking) => {
-        if (booking._id != id) {
-          const st = new dayjs(booking.starttime);
-          const en = new dayjs(booking.endtime);
-          const start = new dayjs(bookingDetails.starttime);
-          const end = new dayjs(bookingDetails.endtime);
-          const stDiff = start.diff(st, "h") / 24;
-          const enDiff = end.diff(en, "h") / 24;
-          const startDiff = start.diff(en, "h") / 24;
-          const endDiff = end.diff(st, "h") / 24;
-          if (
-            (stDiff <= 0 && endDiff >= 0) ||
-            (stDiff <= 0 && startDiff >= 0) ||
-            (endDiff >= 0 && enDiff <= 0)
-          ) {
-            console.log("Booking overlap");
-            overlap = true;
-          }
+  let overlap = false;
+  const room = await Room.findOne({ room_no: bookingDetails.room_no });
+  if (room.allBookings.length > 0) {
+    room.allBookings.map((booking) => {
+      if (booking._id != id) {
+        const st = new dayjs(booking.starttime);
+        const en = new dayjs(booking.endtime);
+        const start = new dayjs(bookingDetails.starttime);
+        const end = new dayjs(bookingDetails.endtime);
+        const stDiff = start.diff(st, "h") / 24;
+        const enDiff = end.diff(en, "h") / 24;
+        const startDiff = start.diff(en, "h") / 24;
+        const endDiff = end.diff(st, "h") / 24;
+        if (
+          (stDiff <= 0 && endDiff >= 0) ||
+          (stDiff <= 0 && startDiff >= 0) ||
+          (endDiff >= 0 && enDiff <= 0)
+        ) {
+          console.log("Booking overlap");
+          overlap = true;
         }
-      });
-    }
-    if (!overlap) {
-      await Bookings.findOneAndUpdate(
-        { _id: id },
-        {
-          room_id: bookingDetails.room_id,
-          room_no: bookingDetails.room_no,
-          email: bookingDetails.email,
-          starttime: bookingDetails.starttime,
-          endtime: bookingDetails.endtime,
-          price: bookingDetails.price,
-          isBooked: bookingDetails.isBooked,
-        }
-      );
-      return NextResponse.json({ message: "Details Updated" });
-    } else {
-      return NextResponse.json({ status: 404, statusText: "Room Overlapping" });
-    }
-  } else {
+      }
+    });
+  }
+  if (!overlap) {
     await Bookings.findOneAndUpdate(
+      { _id: id },
       {
-        _id: id,
-      },
-      {
+        room_id: bookingDetails.room_id,
+        room_no: bookingDetails.room_no,
+        email: bookingDetails.email,
+        starttime: bookingDetails.starttime,
+        endtime: bookingDetails.endtime,
+        price: bookingDetails.price,
         isBooked: bookingDetails.isBooked,
       }
     );
-    return NextResponse.json({ message: "Booking Cancelled" });
+    return NextResponse.json({ message: "Details Updated" });
+  } else {
+    return NextResponse.json({ status: 404, statusText: "Room Overlapping" });
   }
+}
+export async function DELETE(request) {
+  const body = await request.json();
+  const { _id, room_id } = body;
+  const room = await Room.findById(room_id);
+
+  let date = new Date().toLocaleDateString;
+  date = new dayjs(date);
+  const startDate = new dayjs(body.starttime);
+  const refund = startDate.diff(date, "h");
+  room.allBookings = room.allBookings.filter((booking) => booking._id !== _id);
+  await room.save();
+  await Bookings.findByIdAndDelete(_id);
+  return NextResponse.json({
+    status: 201,
+    statusText: `${
+      refund < 24 ? "No Refund" : refund <= 48 ? "25% Refund" : "50% Refund"
+    }`,
+  });
 }
--- app/bookings/page.js
+++ app/bookings/page.js
@@ -1,23 +1,118 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import BookingCard from "../components/BookingCard";
+import { useEffect, useState, useRef } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-
+import { InputText } from "primereact/inputtext";
+import { Calendar } from "primereact/calendar";
+import { Toast } from "primereact/toast";
+import { ContextMenu } from "primereact/contextmenu";
 export default function BookingsPage() {
   const [bookings, setBookings] = useState(null);
+  const [selectedBooking, setSelectedBooking] = useState(null);
+  const toast = useRef(null);
+  const cm = useRef(null);
   useEffect(() => {
     fetch("http://localhost:3000/api/getBookings")
       .then((res) => res.json())
       .then((data) => setBookings(data.bookings));
   }, []);
+  const menuModel = [
+    {
+      label: "Delete",
+      icon: "pi pi-fw pi-times",
+      command: () => deleteBooking(selectedBooking),
+    },
+  ];
+  const deleteBooking = (booking) => {
+    let _bookings = [...bookings];
+    fetch("http://localhost:3000/api/addBookings", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        _bookings = _bookings.filter((p) => p._id !== booking._id);
+        toast.current.show({
+          severity: "info",
+          summary: "Booking Cancelled",
+          detail: data.statusText,
+        });
+        setBookings(_bookings);
+      });
+  };
+  const textEditor = (options) => {
+    return (
+      <InputText
+        type="text"
+        value={options.value}
+        onChange={(e) => options.editorCallback(e.target.value)}
+      />
+    );
+  };
+  const rowClassName = (data) => (isSelectable(data) ? "" : "p-disabled");
+
+  const dateEditor = (options) => {
+    return (
+      <Calendar
+        dateFormat="dd/mm/yy"
+        value={options.value}
+        onChange={(e) => {
+          const date = new Date(e.value).toLocaleDateString();
+          return options.editorCallback(date);
+        }}
+        showIcon
+      />
+    );
+  };
+  const isSelectable = (data) => data.isBooked === true;
+  const isRowSelectable = (event) =>
+    event.data ? isSelectable(event.data) : true;
+  const onRowEditComplete = (e) => {
+    let _bookings = [...bookings];
+    let { newData, index } = e;
+    fetch("http://localhost:3000/api/addBookings", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: newData._id,
+        bookingDetails: {
+          email: newData.email,
+          starttime: newData.starttime,
+          endtime: newData.endtime,
+          price: newData.price,
+          isBooked: newData.isBooked,
+          room_id: newData.room_id,
+          room_no: newData.room_no,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        toast.current.show({
+          severity: "success",
+          summary: data.message,
+          detail: "Booking Details Updated",
+          life: 3000,
+        });
+        _bookings[index] = newData;
+
+        setBookings(_bookings);
+      });
+  };
   return (
     <>
       {bookings == null ? (
         <>No Bookings to be shown!</>
       ) : (
         <div className="card">
+          <Toast ref={toast} />
+          <ContextMenu
+            model={menuModel}
+            ref={cm}
+            onHide={() => setSelectedBooking(null)}
+          />
           <DataTable
             value={bookings}
             tableStyle={{ minWidth: "50rem" }}
@@ -26,30 +121,43 @@ export default function BookingsPage() {
             rows={5}
             editMode="row"
             dataKey="id"
+            onRowEditComplete={onRowEditComplete}
+            isDataSelectable={isRowSelectable}
+            selection={selectedBooking}
+            selectionMode="single"
+            onSelectionChange={(e) => setSelectedBooking(e.value)}
+            rowClassName={rowClassName}
+            onContextMenu={(e) => cm.current.show(e.originalEvent)}
+            contextMenuSelection={selectedBooking}
+            onContextMenuSelectionChange={(e) => setSelectedBooking(e.value)}
           >
             <Column
               field="room_no"
               header="Number"
               sortable
               style={{ width: "25%" }}
+              editor={(options) => textEditor(options)}
             ></Column>
             <Column
               field="email"
               header="E-Mail"
               sortable
               style={{ width: "25%" }}
+              editor={(options) => textEditor(options)}
             ></Column>
             <Column
               field="starttime"
               header="Start Date"
               sortable
               style={{ width: "25%" }}
+              editor={(options) => dateEditor(options)}
             ></Column>
             <Column
               field="endtime"
               header="End Date"
               sortable
               style={{ width: "25%" }}
+              editor={(options) => dateEditor(options)}
             ></Column>
             <Column
               field="price"
